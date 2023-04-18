056e67684e3d666f28da205fcc67115562e0cc41
ma-abasian <abasianmohammadali@gmail.com>
2023-04-19 00:34:28 +0330

    fix(Home): Fix bugs in home page and make transition of menu

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,82 +1,79 @@
 function burgerMenuHandler() {
-  var pageBody = document.getElementById("page-content");
-  var myTopnav = document.getElementById("myTopnav");
-  pageBody.style.marginTop = "0";
-  if (myTopnav.className === "topnav") {
-    myTopnav.className += " responsive";
-    if (window.innerWidth <= 426) {
-      pageBody.style.marginTop = "300px";
+    var myTopnav = document.getElementById("myTopnav");
+    console.log(myTopnav.style.height)
+    if (myTopnav.style.height == "0px" || myTopnav.style.height == "") {
+        myTopnav.style.height = "340px";
+        console.log(myTopnav.style.height)
+    } else {
+        myTopnav.style.height = "0";
     }
-  } else {
-    myTopnav.className = "topnav";
-  }
 }
 
 window.onscroll = function () {
-  var header = document.getElementById("header-container");
-  if (window.pageYOffset > 80) {
-    header.style.position = "sticky";
-  } else {
-    header.style.position = "relative";
-  }
+    var header = document.getElementById("header-container");
+    if (window.pageYOffset > 80) {
+        header.style.position = "sticky";
+    } else {
+        header.style.position = "relative";
+    }
 };
 
 function activeMenuHandler(activeTabName) {
-  var home = document.getElementById("home");
-  var aboutUs = document.getElementById("about-us");
-  var carLeasing = document.getElementById("car-leasing");
-  var details = document.getElementById("details");
-  var contact = document.getElementById("contact");
-  var buyNow = document.getElementById("buy-now");
-  switch (activeTabName) {
-    case "about-us":
-      home.className = "";
-      aboutUs.className = "active";
-      carLeasing.className = "";
-      details.className = "";
-      contact.className = "";
-      buyNow.className = "";
-      break;
-    case "car-leasing":
-      home.className = "";
-      aboutUs.className = "";
-      carLeasing.className = "active";
-      details.className = "";
-      contact.className = "";
-      buyNow.className = "";
-      break;
-    case "details":
-      home.className = "";
-      aboutUs.className = "";
-      carLeasing.className = "";
-      details.className = "active";
-      contact.className = "";
-      buyNow.className = "";
-      break;
-    case "contact":
-      home.className = "";
-      aboutUs.className = "";
-      carLeasing.className = "";
-      details.className = "";
-      contact.className = "active";
-      buyNow.className = "";
-      break;
-    case "buy-now":
-      home.className = "";
-      aboutUs.className = "";
-      carLeasing.className = "";
-      details.className = "";
-      contact.className = "";
-      buyNow.className = "active";
-      break;
+    var home = document.getElementById("home");
+    var aboutUs = document.getElementById("about-us");
+    var carLeasing = document.getElementById("car-leasing");
+    var details = document.getElementById("details");
+    var contact = document.getElementById("contact");
+    var buyNow = document.getElementById("buy-now");
+    switch (activeTabName) {
+        case "about-us":
+            home.className = "";
+            aboutUs.className = "active";
+            carLeasing.className = "";
+            details.className = "";
+            contact.className = "";
+            buyNow.className = "";
+            break;
+        case "car-leasing":
+            home.className = "";
+            aboutUs.className = "";
+            carLeasing.className = "active";
+            details.className = "";
+            contact.className = "";
+            buyNow.className = "";
+            break;
+        case "details":
+            home.className = "";
+            aboutUs.className = "";
+            carLeasing.className = "";
+            details.className = "active";
+            contact.className = "";
+            buyNow.className = "";
+            break;
+        case "contact":
+            home.className = "";
+            aboutUs.className = "";
+            carLeasing.className = "";
+            details.className = "";
+            contact.className = "active";
+            buyNow.className = "";
+            break;
+        case "buy-now":
+            home.className = "";
+            aboutUs.className = "";
+            carLeasing.className = "";
+            details.className = "";
+            contact.className = "";
+            buyNow.className = "active";
+            break;
 
-    default:
-      home.className = "active";
-      aboutUs.className = "";
-      carLeasing.className = "";
-      details.className = "";
-      contact.className = "";
-      buyNow.className = "";
-      break;
-  }
+        default:
+            home.className = "active";
+            aboutUs.className = "";
+            carLeasing.className = "";
+            details.className = "";
+            contact.className = "";
+            buyNow.className = "";
+            break;
+    }
 }
